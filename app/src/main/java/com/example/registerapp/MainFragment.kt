7a614025e042b439959c20e0584a7a4398cf20d8
Roman Ambartsumyan <R9854334307@mail.ru>
2021-08-25 13:35:25 +0300

    fragment is done

--- app/src/main/java/com/example/registerapp/MainFragment.kt
+++ app/src/main/java/com/example/registerapp/MainFragment.kt
@@ -1,11 +1,60 @@
 package com.example.registerapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.registerapp.databinding.FragmentMainBinding
+import com.example.registerapp.utils.*
+import com.google.android.material.textfield.TextInputLayout
 
 class MainFragment : Fragment(R.layout.fragment_main) {
-    
+    private val binding: FragmentMainBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            button.setOnClickListener {
+                val secondNameIsValid =
+                    validField(textInputLayoutSecondName)
+                val nameIsValid = validField(textInputLayoutName)
+                val patronymicIsValid = validField(textInputLayoutPatronymic)
+                val emailIsValid = validEmail(textInputLayoutEmail)
+                val passwordIsValid =
+                    validField(textInputLayoutPassword) &&
+                            validField(textInputLayoutRepeatPassword) &&
+                            arePasswordsTheSame(
+                                textInputLayoutPassword,
+                                textInputLayoutRepeatPassword
+                            )
+
+                addTextChangeListener(textInputLayoutSecondName)
+                addTextChangeListener(textInputLayoutName)
+                addTextChangeListener(textInputLayoutPatronymic)
+                addEmailChangeListener(textInputLayoutEmail)
+                addTextChangeListener(textInputLayoutPassword)
+                addTextChangeListener(textInputLayoutRepeatPassword)
+                addRepeatPasswordChangeListener(
+                    textInputLayoutPassword,
+                    textInputLayoutRepeatPassword
+                )
+            }
+        }
+    }
+
+    @SuppressLint("ResourceType")
+    private fun arePasswordsTheSame(
+        passwordLayout: TextInputLayout,
+        repeatPasswordLayout: TextInputLayout
+    ): Boolean {
+        return if (passwordLayout.editText?.text.toString() == repeatPasswordLayout.editText?.text.toString()) {
+            true
+        } else {
+            repeatPasswordLayout.error = getStringRes(R.string.passwords_are_not_the_same)
+            false
+        }
+    }
 }
+
+
---
+++ app/src/main/java/com/example/registerapp/utils/FragmetUtils.kt
@@ -0,0 +1,80 @@
+package com.example.registerapp.utils
+
+import android.annotation.SuppressLint
+import android.util.Patterns
+import androidx.annotation.LayoutRes
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.example.registerapp.R
+import com.google.android.material.textfield.TextInputLayout
+
+@SuppressLint("ResourceType")
+fun Fragment.getStringRes(@LayoutRes res: Int): String {
+    return resources.getString(res)
+}
+
+fun Fragment.validField(layout: TextInputLayout): Boolean {
+    return if (layout.editText?.text!!.isBlank()) {
+        layout.error = getString(R.string.required_field)
+        true
+    } else {
+        layout.error = null
+        false
+    }
+}
+
+@SuppressLint("ResourceType")
+fun Fragment.validEmail(layout: TextInputLayout): Boolean {
+    val text = layout.editText?.text!!
+    return if (text.isBlank() || Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
+        layout.error = getString(R.string.required_field)
+        true
+    } else {
+        if (!Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
+            layout.error = getStringRes(R.string.incorrect_email)
+        } else {
+            layout.error = null
+        }
+        false
+    }
+}
+
+@SuppressLint("ResourceType")
+fun Fragment.addTextChangeListener(layout: TextInputLayout) {
+    layout.editText?.doOnTextChanged { text, _, _, _ ->
+        if (text!!.isBlank()) {
+            layout.error = getStringRes(R.string.required_field)
+        } else {
+            layout.error = null
+        }
+    }
+}
+
+@SuppressLint("ResourceType")
+fun Fragment.addEmailChangeListener(layout: TextInputLayout) {
+    layout.editText?.doOnTextChanged { text, _, _, _ ->
+        if (text!!.isBlank()) {
+            layout.error = getStringRes(R.string.required_field)
+        } else {
+            if (!Patterns.EMAIL_ADDRESS.matcher(layout.editText?.text!!).matches()) {
+                layout.error = getStringRes(R.string.incorrect_email)
+            } else {
+                layout.error = null
+            }
+        }
+    }
+}
+
+@SuppressLint("ResourceType")
+fun Fragment.addRepeatPasswordChangeListener(
+    layoutPassword: TextInputLayout,
+    layoutRepeatPassword: TextInputLayout
+) {
+    layoutRepeatPassword.editText?.doOnTextChanged { text, _, _, _ ->
+        if (text.toString() != layoutPassword.editText?.text.toString()) {
+            layoutRepeatPassword.error = getStringRes(R.string.passwords_are_not_the_same)
+        } else {
+            layoutRepeatPassword.error = null
+        }
+    }
+}
